247ee73b19a4c34ea3b11f585324b22979ddee0d
Tomoaki imac-2019i9 <tomoaki.nagahara@gmail.com>
2020-05-23 19:31:31 +0900

    New: testcase > action.php > testcase.js > testcase.css

--- testcase/testcase.js
+++ testcase/testcase.js
@@ -1,4 +1,18 @@
-(function(){
-	//	...
-	console.log('testcase', $OP ? true:false);
-})();
+//<?php
+/** op-unit-webpack:/testcase/testcase.js
+ *
+ * @creation  2020-05-22
+ * @version   1.0
+ * @package   op-unit-webpack
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//?>
+
+//	...
+var span = document.createElement('span');
+	span.innerText = 'Successful';
+
+//	...
+var div  = document.querySelector('.js');
+	div.appendChild(span);
