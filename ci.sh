192ea21ec81a82c915009ed58dea25a3b6ef4f4a
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2022-11-14 19:54:13 +0900

    CI: ci.sh

---
+++ ci.sh
@@ -0,0 +1,13 @@
+
+ ## op-unit-webpack:/ci.sh
+ #
+ # Call from git pre-push
+ #
+ # @created   2022-11-14
+ # @version   1.0
+ # @package   op-unit-webpack
+ # @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ # @copyright Tomoaki Nagahara All right reserved.
+
+php ../../../ci.php display=0
+
