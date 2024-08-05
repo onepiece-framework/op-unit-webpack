bbf118ac5268754b38978836166e2875bf819241
TomoakiMacBookAirM1 <TomoakiNagahara@github.com>
2024-08-06 03:14:28 +0900

    New: include > GetExtensionFromURL.php

---
+++ include/GetExtensionFromURL.php
@@ -0,0 +1,47 @@
+<?php
+/** op-unit-webpack:/include/GetExtensionFromURL.php
+ *
+ * @created    2024-08-06
+ * @version    2.0
+ * @package    op-unit-webpack
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP\UNIT\WEBPACK;
+
+//	Calculate file type from Request URL.
+$uri = $_SERVER['REQUEST_URI'];
+
+//	Remove URL Query : /webpack/index.css?layout=flexbox --> /webpack/index.css
+if( $pos = strpos($uri, '?') ){
+	$uri = substr($uri, 0, $pos);
+}
+
+//	Find the file extension.
+if( $pos = strrpos($uri, '.') ){
+	$ext = substr($uri, $pos -1);
+}else{
+	//	Remove slash from tail : /webpack/css/ --> /webpack/css
+	$uri = rtrim($uri, '/');
+	//	Find the file extension from directory : /webpack/css --> css
+	if( $pos = strrpos($uri, '/') ){
+		$ext = substr($uri, $pos +1);
+	}
+}
+
+//	If not found.
+if( empty($ext) ){
+	OP()->Notice("The extension could not be found. `{$_SERVER['REQUEST_URI']}`");
+}
+
+//	Return extension.
+return $ext ?? null;
